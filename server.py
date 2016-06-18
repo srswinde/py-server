d83bb629437c738501f57899cf338f6a85722e2d
scott <scottswindell@email.arizona.edu>
2016-06-17 18:13:02 -0700

    Added a tryagain param to the Server constructor. If this is nonzero the Server object will, after failing to open a socket, will look for processes or open sockets that could be causing the problem. If the offending open socket has a process associated with it and the process is stopped we kill the process and wait 10 seconds to try again. If not we wait 60 seconds in hopes the socket will close. Usually it does.
    We only try again one time.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import time
 import os
 import subprocess
 import sys
-
+import psutil
 
 #Class to parse input from socket and send output
 #opened by Server class
@@ -62,7 +62,7 @@ class Server:
  an instance of the client class is made
  To parse input and send output. """
  
-	def __init__( self, port, handler=Client, killSwitch=False ):
+	def __init__( self, port, handler=Client, killSwitch=False, tryagain=False ):
 		self.host = ''
 		self.port = port
 		self.backlog = 5
@@ -72,6 +72,7 @@ class Server:
 		self.handler = handler
 		self.running = True
 		self.killSwitch = killSwitch
+		self.tryagain = tryagain
 		if killSwitch:
 			self.killLock = threading.Lock()
 
@@ -82,22 +83,57 @@ class Server:
 		try:
 			self.server = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 			self.server.bind( ( self.host, self.port ) )
-			self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			self.server.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 			self.server.listen( 5 )
-		except socket.error, ( value, message ):
+			
+			return 0
+		except socket.error as err :
 		
 			if self.server:
-				self.server.close()
-				print "Could not open socket: " + message
-				sys.exit( 1 )
 
-		
+				self.server.close()
+				print "Could not open socket: " + err.strerror
+
+				t0 = time.time()
+				if err.errno == 98 and self.tryagain:		
+								
+					print "checking for lost socket..."			
+					for conn in psutil.net_connections():
+						if conn.laddr[1] == self.port:
+							
+							print "Found socket {}".format(conn)
+							if conn.pid:
+								psutil.Process(conn.pid).kill()
+								return 10
+							else:
+								return 60
+							
+				
+			return -1
 	def test(self):
 		print "TESTING"
+		
 	def run( self ):
-		self.open_socket()
-		
-		
+		resp = self.open_socket()
+		if resp < 0:
+			return 
+		elif resp > 0:
+			print "Process is trying to use this address, giving them {}s to close".format(resp)
+			t0 = time.time()
+			while ( time.time() - t0 ) < resp:
+				sys.stdout.write( str(int(time.time()-t0)) )
+				sys.stdout.flush()
+				time.sleep(0.5)
+				sys.stdout.write('\r')
+			print
+			resp = self.open_socket()
+			
+			if resp != 0:
+				print "didn't work check to see who is using this socket."
+				return 
+			else:
+				print "Success"
+				
 		input = [self.server ]
 	
 		while self.running:
@@ -127,7 +163,28 @@ class Server:
 
 		self.server.close()
 		for c in self.threads:
+			try:
+				c.client.close()
+			except socket.error:
+				pass	
+		
 			c.join()
 
 	def kill( self ):
 		self.running = 0
+		
+		
+if __name__ == "__main__":
+	usage = "usage: server.py [portnum]  \n Like: server.py 6543"
+
+	if len(sys.argv) != 2:
+		print usage
+	else:
+		try:
+			s=Server( int(sys.argv[1]), tryagain=True )
+			s.run()
+		except NameError:
+			print usage
+			
+	
+
